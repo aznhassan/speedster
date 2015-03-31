f592992ce216a0f240149032ee6dbdc22c325d31
aznhassan <hassan_sufi@brown.edu>
2015-03-30 20:47:50 -0400

    Imported Writer and Reader interface.

---
+++ src/main/java/edu/brown/cs/mmth/fileIo/Readable.java
@@ -0,0 +1,28 @@
+package edu.brown.cs.mmth.fileIo;
+
+import java.util.List;
+
+/**
+ * Implementing this interface allows our readers to fill the
+ * objects of the implementing class with data from disk.
+ * @author hsufi
+ *
+ */
+public interface Readable {
+
+  /**
+   * This allows us to update the id of the object
+   * to that of the file from which we are reading.
+   */
+  void setId();
+
+  /** The given data will be of the form
+   * field_name: field_value.
+   * Implementing objects should read the parameters and update
+   * their fields accordingly. The implementing objects are responsible
+   * for checking for corrupt data themselves.
+   * @param fields - T
+   */
+  void updateFields(List<String> fields);
+
+}
