edb43280dc4aaaa3c1e8d4a7e10f2bfadc64ea33
Hassan Sufi <hassan_sufi@brown.edu>
2015-04-24 13:09:15 -0400

    More fixes to APIHandler

--- src/main/java/edu/brown/cs/mmth/speedster/ApiHandler.java
+++ src/main/java/edu/brown/cs/mmth/speedster/ApiHandler.java
@@ -1,7 +1,9 @@
 package edu.brown.cs.mmth.speedster;
 
-
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +14,6 @@ import com.google.gson.Gson;
 import edu.brown.cs.mmth.fileIo.CSSSheetMaker;
 import edu.brown.cs.mmth.fileIo.NoteReader;
 import edu.brown.cs.tbhargav.tries.Word;
-
 import spark.ModelAndView;
 import spark.QueryParamsMap;
 import spark.Request;
@@ -39,7 +40,9 @@ public final class ApiHandler {
   private ApiHandler() {
   }
 
-  /** Loads metadata associated with all notes.
+  /**
+   * Loads metadata associated with all notes.
+   * 
    * @author hsufi
    *
    */
@@ -47,18 +50,17 @@ public final class ApiHandler {
     @Override
     public ModelAndView handle(final Request req, final Response res) {
       QueryParamsMap qm = req.queryMap();
-      
-      //Grab the note with this id from the db
-      Map<String, Object> variables =
-              ImmutableMap.of(
-                      "title", "Welcome home");
+
+      // Grab the note with this id from the db
+      Map<String, Object> variables = ImmutableMap.of("title", "Welcome home");
       return new ModelAndView(variables, "main.ftl");
     }
   }
 
   /**
-   * Handles updating notes per folder when new notes are added by the
-   * user on the main page.
+   * Handles updating notes per folder when new notes are added by the user on
+   * the main page.
+   * 
    * @author sm15
    */
   public static class UpdateNotes implements Route {
@@ -66,14 +68,14 @@ public final class ApiHandler {
     public Object handle(final Request req, final Response res) {
       QueryParamsMap qm = req.queryMap();
       String notes = qm.value("notes");
-      Map<String, Object> variables =
-              ImmutableMap.of(
-                      "title", "Welcome home");
+      Map<String, Object> variables = ImmutableMap.of("title", "Welcome home");
       return new ModelAndView(variables, "main.ftl");
     }
   }
 
-  /** Generates autocorrect suggestions for the given word.
+  /**
+   * Generates autocorrect suggestions for the given word.
+   * 
    * @author tbhargav
    *
    */
@@ -85,8 +87,8 @@ public final class ApiHandler {
       String inputText = qm.value("text_box");
 
       // String parsing to get neat suggestions.
-      String sansPunct = edu.brown.cs.tbhargav.autocorrect.Main
-          .stripPunctuation(inputText);
+      String sansPunct =
+          edu.brown.cs.tbhargav.autocorrect.Main.stripPunctuation(inputText);
       String[] arr = sansPunct.split(" ");
       String word = arr[arr.length - 1];
       String prevWord = "";
@@ -98,12 +100,12 @@ public final class ApiHandler {
       }
 
       if (!withoutWord.toString().isEmpty()) {
-        prevWord = withoutWord.toString().split(" ")[withoutWord.toString()
-                                                     .split(" ").length - 1];
+        prevWord =
+            withoutWord.toString().split(" ")[withoutWord.toString().split(" ").length - 1];
       }
 
-      List<Word> suggs = edu.brown.cs.tbhargav.autocorrect.Main
-          .suggGenAndRanker(
+      List<Word> suggs =
+          edu.brown.cs.tbhargav.autocorrect.Main.suggGenAndRanker(
               edu.brown.cs.tbhargav.autocorrect.Main.getGlobalTrie(), word,
               prevWord);
 
@@ -122,36 +124,41 @@ public final class ApiHandler {
       // Adding suggestions in manually!
       Map<String, Object> variables;
 
-      variables = ImmutableMap.of("title", "Hollywood Connections", "message",
-          " ", "orig", inputText, "suggs", sb.toString());
+      variables =
+          ImmutableMap.of("title", "Hollywood Connections", "message", " ",
+              "orig", inputText, "suggs", sb.toString());
 
       return gson.toJson(variables);
 
     }
   }
 
-  /** Loads the note given by the id and then runs that
-   *  note on it's own page.
+  /**
+   * Loads the note given by the id and then runs that note on it's own page.
+   * 
    * @author hsufi
    *
    */
-  public static class GetNote implements TemplateViewRoute  {
+  public static class GetNote implements TemplateViewRoute {
     @Override
     public ModelAndView handle(final Request req, final Response res) {
+      Map<String, Object> problem =
+          ImmutableMap.of("title", "Speedster", "content", "Improper note id");
       int id;
       try {
         id = Integer.parseInt(req.params("id"));
       } catch (NumberFormatException e) {
-        Map<String, Object> variables =
-            ImmutableMap.of(
-                    "title", "Speedster",
-                    "content", "Improper note id");
-            return new ModelAndView(variables, "error.ftl");
+        return new ModelAndView(problem, "error.ftl");
       }
       String subject = req.params("folder");
+      try {
+        subject = URLDecoder.decode(subject, "UTF-8");
+      } catch (UnsupportedEncodingException e) {
+        return new ModelAndView(problem, "error.ftl");
+      }
       Collection<Note> notes = NoteReader.readNotes(subject);
       Note returnNote = null;
-      for (Note note: notes) {
+      for (Note note : notes) {
         if (note.getId() == id) {
           returnNote = note;
           break;
@@ -160,23 +167,20 @@ public final class ApiHandler {
       Map<String, Object> variables;
       if (returnNote != null) {
         variables =
-            ImmutableMap.of(
-                "title", "Speedster",
-                "note", returnNote.getTextData(),
-                "customCss", subject);
+            ImmutableMap.of("title", "Speedster", "note",
+                returnNote.getTextData(), "customCss", subject);
       } else {
         variables =
-            ImmutableMap.of(
-                "title", "Speedster",
-                "note", "",
-                "customCss", subject);
+            ImmutableMap.of("title", "Speedster", "note", "", "customCss",
+                subject);
       }
       return new ModelAndView(variables, "note.ftl");
     }
   }
 
-  /** Updates the stylesheet of the current subject
-   *  with the given rules.
+  /**
+   * Updates the stylesheet of the current subject with the given rules.
+   * 
    * @author hsufi
    */
   public static class UpdateCSS implements Route {
@@ -195,8 +199,10 @@ public final class ApiHandler {
     }
   }
 
-  /** Grabs the next flash card to display to the user
-   * based on the data from each flashcard.
+  /**
+   * Grabs the next flash card to display to the user based on the data from
+   * each flashcard.
+   * 
    * @author hsufi
    */
   public static class GetNextFlashCard implements Route {
@@ -209,9 +215,10 @@ public final class ApiHandler {
     }
   }
 
-  /** Updates the meta-data of the given flashcard, such as
-   *  adding to the number of right and wrongs as well as updating
-   *  the time stamp of the flashcard.
+  /**
+   * Updates the meta-data of the given flashcard, such as adding to the number
+   * of right and wrongs as well as updating the time stamp of the flashcard.
+   * 
    * @author hsufi
    */
   public static class UpdateFlashCard implements Route {
