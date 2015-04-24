5a91c4741d96d98d98986db062fec06104fe843a
Tushar Bhargava <tushar_bhargava@brown.edu>
2015-04-24 14:11:16 -0400

    Hassan blamed me for his faults. Et tu Brutus?

--- src/main/java/edu/brown/cs/mmth/speedster/Main.java
+++ src/main/java/edu/brown/cs/mmth/speedster/Main.java
@@ -104,6 +104,8 @@ public final class Main {
         writer.write("" + value); // starting the count.
       } catch (IOException e) {
         System.err.println("ERROR: " + e.getMessage());
+        // TODO: Remember you have to remember stack trace.
+        e.printStackTrace();
         System.exit(1);
       }
     }
@@ -158,6 +160,8 @@ public final class Main {
           }
         } catch (NumberFormatException e1) {
           System.err.println("ERROR: " + e1.getMessage());
+          // TODO: Remember you have to remember stack trace.
+          e1.printStackTrace();
           return -1;
         }
       }
