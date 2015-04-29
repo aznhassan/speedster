f6c590c50a3ef714f2dc9f696aeaca88b6843917
nighttime <softwareatnight@gmail.com>
2015-04-29 15:00:30 -0400

    deactivated rules in the title. take THAT jake

--- src/main/resources/static/js/note.js
+++ src/main/resources/static/js/note.js
@@ -210,12 +210,19 @@ function stylize(correcting) {
     res = escapeHTML(res);
 
     // \u200b -> <br>\u200b
-    res = res.replace(/\u200b/g, '<br>\u200b');
+    res = res.replace(/\u200b/g, NEWLINE);
+
+    var idx = res.indexOf(NEWLINE);
+    //console.log(idx);
+    var title = idx == -1 ? res : res.substring(0, idx);
+    res = idx == -1 ? '' : res.substring(idx);
+
 
     // TITLE
-    res = res.replace(/^(.*?)(<br>\u200b|$)/, function(x, a, b) {
-        return '<div class="title">' + a + '</div>' + b;
-    });
+    // res = res.replace(/^(.*?)(<br>\u200b|$)/, function(x, a, b) {
+    //     return '<div class="title">' + a + '</div>' + b;
+    // });
+    title = '<div class="title">' + title + '</div>';
 
     // NOTE:
     res = res.replace(/(note:)(.*?)(<br>\u200b|$)/gi, function(x, a, b, c) {
@@ -253,6 +260,8 @@ function stylize(correcting) {
     // res = res.replace(/(<br>\u200b<br>\u200b)([^<\u200b]+?)(<br>\u200b|$)/gi, function(x, a, b, c) {
     //     return a + '<span class="section">' + b + '</span>' + c;
     // });
+
+    res = title + res;
 
     // Decode HTML special chars
     res = unescapeHTML(res);
@@ -351,7 +360,7 @@ function compileUserRule(rule) {
 	var afterEndSeq = (rule.after && rule.after.endSeq ? '(.*?)' + '(' + regEsc(rule.after.endSeq) + '|$)' : '');
 	var afterStyle = (rule.after ? rule.after.style || '' : '');
 	var containerStyle = (rule.container ? rule.container.style : '');
-	var newline1 = rule.trigger.endSeq && rule.trigger.endSeq == '<br>\u200b' && !rule.after;
+	var newline1 = containerStyle && rule.trigger.endSeq && rule.trigger.endSeq == '<br>\u200b' && !rule.after;
   var newline2 = containerStyle && rule.after && rule.after.endSeq == '<br>\u200b';
 
 
@@ -431,7 +440,7 @@ function save() {
       'noteid': urlparts[3],
       'subject': decodeURIComponent(urlparts[2])
     }
-    console.log(params);
+    //console.log(params);
 
     $.post("/updateNote", params, function() {
         // merp...
